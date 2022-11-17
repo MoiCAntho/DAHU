202b102fe3cf01a910f2dd1813bed4dc041a6ca3
MoiCAntho <profantho@mail.fr>
2022-11-17 18:46:48 +0100

    créat des fichiers necessaire à la librairie

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+#A finir
+
+setup(
+    name = "Librairie"
+    version = "0.1.0"
+    author = "Virgile Cheminot, Anthony Segura"
+    url = "https://github.com/MoiCAntho/Librairie-python"
+)
