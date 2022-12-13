50e23d7d0a2381477cbffc1cfc58a06671cae3e4
MoiCAntho <profantho@mail.fr>
2022-12-13 19:03:11 +0100

    réglages des fx simp div et int

--- Librairie/maths.py
+++ Librairie/maths.py
@@ -13,38 +13,38 @@ import random as r
 class Expression :
 
     def __init__(self,expr,var,cte = None) : #Création d'une expression à l'aide d'un str (Faire les filtres) !
-        c = {"var" : var ,"cte" : cte}
-        self.expr = expr
-        self.Expression = g.giac(expr)
+        self.c = {"var" : var ,"cte" : cte}
+        self.Expr = g.giac(expr)
+        self.sexpr = str(self.Expr)
 
     def __getitem__(self,index) :
-        return self.Expression[index]
+        return self.Expr[index]
 
     def __setitem__(self,index,valeur) :
-        self.Expression[index] = valeur
+        self.Expr[index] = valeur
     
     def __str__(self) :
-        return self.expr
+        return self.sexpr
 
     def __len__(self):
-        return len(self.expr)
+        return len(self.sexpr)
     
     def __repr__(self) :
         a = str(self)
         return self
 
     def __add__(self,autre) :
-        a = self.expr
-        b = autre.expr
+        a = self.sexpr
+        b = autre.sexpr
         a = g.giac(a)
         b = g.giac(b)
         c = a + b
-        self.Expression = c
-        self.expr = str(c)
+        self.Expr = c
+        self.sexpr = str(c)
         return self
 
     def subs(self,var_1,var_2) :
-        a = str(self.expr)
+        a = str(self.sexpr)
         b = a.replace(str(var_1),str(var_2))
         return b
     
@@ -53,15 +53,24 @@ class Expression :
     
     ## Calcul infinitesimal : derivees, integrales, limites ...##
 
-    def deriv(self,var) :
-        a = g.giac(self.expr)
-        return g.diff(a,var)
+    def deriv(self,var) : 
+        self.Expr = g.diff(self.Expr,var)
+        self.sexpr = str(self.Expr)
+        return self
     
     def int(self,var,a=None,b=None):
         if a == None and b == None :
-            return g.int(g.giac(self.expr,var))
+            self.Expr = g.int(self.Expr,var)
+            self.sexpr = str(self.Expr)
+            return self
         else :
-            return g.int(g.giac(self.expr,var,a,b))
+            pass
+            #return g.int(self.Expr,var,a,b)
+
+    def simp(self) :
+        self.Expr = g.simplify(self.Expr)
+        self.sexpr = str(self.Expr)
+        return self
 
 ## Definition d'objets généraux ##
 
@@ -111,7 +120,7 @@ class Matrice :
             else :
                 er.Error_ma_6()
         else :
-            er.Error_ma_2(ty="Matrice")
+            pass
 
     def __iadd__(self,autre) : #Utilisation de l'operateur +=
         if isinstance(autre,Matrice) :
@@ -151,7 +160,7 @@ class Matrice :
                             x += self[i][k] * autre[k][j]
                         mat[i][j] = x
                 return mat
-        elif str(type(autre)) == "<class 'float'>" or str(type(autre)) == "<class 'int'>" : #Matrice par reel
+        elif isinstance(autre,int) or isinstance(autre,float) : #Matrice par reel
             mat = Matrice(self.nbl,self.nbc)
             for i in range(self.nbl) :
                 for j in range(self.nbc) :
@@ -194,88 +203,6 @@ class Matrice :
         else :
             return False
 
-    ## Generations de Matrices particulieres ##
-
-    def randomatrice(lignes,colonnes,xmin=-15,xmax=15) : #Generation d'une matrice aux coefficients aleatoires
-        mat = Matrice(lignes,colonnes)
-        for i in range(lignes) :
-            for j in range(colonnes) :
-                mat[i][j] = r.randint(xmin,xmax)
-        return mat
-
-    def matrice123(lignes,colonnes) : #Generation d'une matrice suivant la suite des entiers naturels !
-        mat = Matrice(lignes,colonnes)
-        t = 1
-        for i in range(lignes) :
-            for j in range(colonnes) :
-                mat[i][j] = t
-                t += 1
-        return mat
-    
-    def matriceid(n) : #Generation de la matrice identite de dimension n !
-        mat = Matrice(n,n)
-        for i in range(n) :
-            for j in range(n) :
-                if i == j :
-                    mat[i][j] = 1
-                else :
-                    mat[i][j] = 0
-        return mat
-
-    def matriceel(element) : #Generation d'une matrice à partir d'une liste de liste d'elements
-        cpt = 0
-        cd = True
-        for i in element :
-            if cpt == 0 :
-                a = len(i)
-                cpt += 1
-            else :
-                if a != len(i) :
-                    cd = False
-        if cd != False :
-            mat = Matrice(len(element),a)
-            for i in range(len(element)) :
-                for j in range(a) :
-                    mat[i][j] = element[i][j]
-            return mat
-        else :
-            er.Error_ma_5()
-
-    def mat_creuse_1(ligne, colonne, valeur,n) : #Matrice creuse contenant 1 valeur à la position (ligne,colonne)
-        mat = Matrice(n,n)
-        mat[ligne][colonne] = valeur
-        return mat
-
-    ## Attention pour utiliser les mel il faut poser la multiplication dans le bon sens mel*mat !! ##
-
-    def mel_permutation_l(ligne_1,ligne_2,n) : #Matrice elementaire de permutation de deux lignes (faire les filtres)
-        mat = Matrice.matriceid(n)
-        for col in range(n) : 
-            mat[ligne_1 -1][col], mat[ligne_2 -1][col] = mat[ligne_2 -1][col], mat[ligne_1 -1][col]
-        return mat
-
-    def mel_dilatation_l(ligne,valeur,n) : #Matrice elementaire de dilatation d'une ligne (faire les filtres)
-        mat = Matrice.matriceid(n)
-        for i in range(n) :
-            if i == ligne-1 :
-                for j in range(n) :
-                    mat[i][j] *= valeur
-        return mat
-
-    def mel_transvection_l(ligne_1,ligne_2,valeur,n) : #Matrice elementaire de transvections de 2 lignes (l1 prend la valeur de l1-valeur*l2) (faire les filtres) !
-        mat = Matrice.matriceid(n)+valeur*Matrice.mat_creuse_1(ligne_1-1,ligne_2-1,1,n)
-        return mat
-
-    # def mel_permutation_c(colonne_1,colonne_2,n) : #A faire
-    #     pass
-    
-    # def mel_dilatation_c(colonne,valeur,n) :
-    #     pass
-
-    # def mel_transvection_c(colonne_1,colonne_2,valeur,n) :
-    #     pass
-
-
     ## Methodes ##
 
     def dim(self): #Renvoie la dimension de la matrice !
@@ -317,13 +244,18 @@ class Matrice :
     def triangsup(self) : #finir et faire les autres matrices elementaires pour col et ligne/colo
         nbl = self.nbl
         nbc = self.nbc
-        cpt = 0
+        cpt1 = 0
+        cpt2 = 0
         for col in range(nbc) :
             a = self[col][col]
-            cpt += 1
+            for i in range(nbc) :
+                self[cpt1][col] /= a
+            cpt1 += 1
             for lig in range(nbl) :
                 if col != lig :
-                    self = Matrice.mel_transvection_l(lig,cpt,a,self.dim()[1])*self
+                    b = self
+                    self = mel_transvection_l(lig,cpt1,-self[cpt1][col],self.dim()[1])*b
+                    print(self)
         return self
 
     def comatrice(self) : #Retourne la matrice de cofacteur d'une matrice (A faire) !
@@ -490,6 +422,93 @@ class Intervalle(Ensemble) :
 
     def __init__(self) :
         pass
+
+
+## Génération de matrices ##
+
+
+def randomatrice(lignes,colonnes,xmin=-15,xmax=15) : #Generation d'une matrice aux coefficients aleatoires
+    mat = Matrice(lignes,colonnes)
+    for i in range(lignes) :
+        for j in range(colonnes) :
+            mat[i][j] = r.randint(xmin,xmax)
+    return mat
+
+def matrice123(lignes,colonnes) : #Generation d'une matrice suivant la suite des entiers naturels !
+    mat = Matrice(lignes,colonnes)
+    t = 1
+    for i in range(lignes) :
+        for j in range(colonnes) :
+            mat[i][j] = t
+            t += 1
+    return mat
+    
+def matriceid(n) : #Generation de la matrice identite de dimension n !
+    mat = Matrice(n,n)
+    for i in range(n) :
+        for j in range(n) :
+            if i == j :
+                mat[i][j] = 1
+            else :
+                mat[i][j] = 0
+    return mat
+
+def matriceel(element) : #Generation d'une matrice à partir d'une liste de liste d'elements
+    cpt = 0
+    cd = True
+    for i in element :
+        if cpt == 0 :
+            a = len(i)
+            cpt += 1
+        else :
+            if a != len(i) :
+                cd = False
+    if cd != False :
+        mat = Matrice(len(element),a)
+        for i in range(len(element)) :
+            for j in range(a) :
+                mat[i][j] = element[i][j]
+        return mat
+    else :
+        er.Error_ma_5()
+
+def mat_creuse_1(ligne, colonne, valeur,n) : #Matrice creuse contenant 1 valeur à la position (ligne,colonne)
+    mat = Matrice(n,n)
+    mat[ligne][colonne] = valeur
+    return mat
+
+    ## Attention pour utiliser les mel il faut poser la multiplication dans le bon sens mel*mat !! ##
+
+def mel_permutation_l(ligne_1,ligne_2,n) : #Matrice elementaire de permutation de deux lignes (faire les filtres)
+    mat = matriceid(n)
+    for col in range(n) : 
+        mat[ligne_1 -1][col], mat[ligne_2 -1][col] = mat[ligne_2 -1][col], mat[ligne_1 -1][col]
+    return mat
+
+def mel_dilatation_l(ligne,valeur,n) : #Matrice elementaire de dilatation d'une ligne (faire les filtres)
+    mat = matriceid(n)
+    for i in range(n) :
+        if i == ligne-1 :
+            for j in range(n) :
+                mat[i][j] *= valeur
+    return mat
+
+def mel_transvection_l(ligne_1,ligne_2,valeur,n) : #Matrice elementaire de transvections de 2 lignes (l1 prend la valeur de l1-valeur*l2) (faire les filtres) !
+    mat = matriceid(n)+valeur*mat_creuse_1(ligne_1-1,ligne_2-1,1,n)
+    print(mat)
+    return mat
+
+    # def mel_permutation_c(colonne_1,colonne_2,n) : #A faire
+    #     pass
+    
+    # def mel_dilatation_c(colonne,valeur,n) :
+    #     pass
+
+    # def mel_transvection_c(colonne_1,colonne_2,valeur,n) :
+    #     pass
+
+
+
 
 ## Fonctions de Calcul algebrique ##
 
