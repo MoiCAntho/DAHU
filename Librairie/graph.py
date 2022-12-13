18a57d2406985280e64dae585656f290e4ac7269
MoiCAntho <profantho@mail.fr>
2022-12-13 19:03:37 +0100

    réglages des fx div simp et int

--- Librairie/graph.py
+++ Librairie/graph.py
@@ -11,16 +11,19 @@ def param(X,Y,param,inter=[-10,10],nbpts=10000): #Faire les filtres
     t = []
     for j in linspace(inter[0],inter[1],nbpts) :
         t.append(j)
+    print(t[0],t[-1])
     x = []
     y = []
     for i in t :
-        x.append(X.eval(param,i))
-        y.append(Y.eval(param,i))
-    print(type(x[1]))
-    fig, axes = plt.subplots()
-    axes.set_xlim(inter[0],inter[1])
-    axes.set_ylim(y[0],y[-1])
-    axes.plot(x,y)
+        a = X.eval(param,i)
+        b = Y.eval(param,i)
+        x.append(a)
+        y.append(b)
+    print(x[0],x[-1],y[0],y[-1])
+    fig, ax = plt.subplots()
+    ax.set_xlim(x[0],x[1])
+    ax.set_ylim(y[0],y[-1])
+    ax.plot(x,y)
     plt.show()
     
 
--- Librairie/test.py
+++ Librairie/test.py
@@ -3,8 +3,9 @@ from graph import param
 
 a = ma.Expression("2t+3",var="t")
 b = ma.Expression("4t^3+6t", var="t")
-print(a+b)
-print(ma.Expression.subs(a,"t","6"))
-print(ma.Expression.deriv(b,"t"))
+# print(a+b)
+# print(a.subs("t","6"))
+# print(b.deriv("t"))
+print(b.int("t").simp())
 
-param(a,b,"t")
+# param(a,b,"t")
