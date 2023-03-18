a8135ec02b65a0f74c0afb8aab8749c1371a1a77
MoiCAntho <profantho@mail.fr>
2023-03-18 17:21:40 +0100

    qq modifs

---
+++ DAHU/elec.py
@@ -0,0 +1,13 @@
+class circuit :
+    def __init__(self) :
+        pass
+
+
+class composant :
+    def __init__(self) :
+        pass
+
+
+def SPICE() : #Convertit le circuit en format SPICE dans fichier .txt
+    pass
+
