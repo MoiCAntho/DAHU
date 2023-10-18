192e152ca14e0538fb70ab54ffa782c78f7c8c9a
MoiCAntho <profantho@mail.fr>
2023-10-18 22:35:51 +0200

    essaie encore

--- setup.py
+++ setup.py
@@ -8,8 +8,11 @@ setup(
     url = "https://github.com/MoiCAntho/DAHU",
     packages = find_packages(),
     install_requires = ["wheel>=0.41.2",
-                        "cython>=3.0.4",
-                        "git+https://gitlab.math.univ-paris-diderot.fr/han/giacpy@master"]
+                        "cython>=3.0.4"
+                        ],
+    dependency_links=[
+        "git+https://gitlab.math.univ-paris-diderot.fr/han/giacpy@master"
+    ],
     description="Package de calcul formel (pour l'instant ;=) )",
     license="MIT",
     python_requires = ">=3.8",
